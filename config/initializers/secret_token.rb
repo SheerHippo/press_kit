1329bd3fe161b2f844744da5272b9d1ae177055e
Dominic Dixon <SheerHippo@gmail.com>
2013-05-08 22:43:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PressKit::Application.config.secret_token = '14b3c3e6299066a279c8c4cc742c1e5c13e84e3bb383576ae47eeeb85e65544b7405bb2db25cb46c45c4b5a89b2d4ab5e261bd878963dfac37cd8948cf13011d'
